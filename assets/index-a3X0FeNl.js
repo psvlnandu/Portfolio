e17fbbd3cc39b3770918a565e47c1d256faa305b
Poorna <103063084+psvlnandu@users.noreply.github.com>
2025-08-10 05:19:39 +0000

    Updates

--- assets/index-a3X0FeNl.js
+++ assets/index-a3X0FeNl.js
@@ -77,7 +77,7 @@ Error generating stack: `+i.message+`
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const Wd=kl("GraduationCap",[["path",{d:"M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z",key:"j76jl0"}],["path",{d:"M22 10v6",key:"1lu8f3"}],["path",{d:"M6 12.5V16a6 3 0 0 0 12 0v-3.5",key:"1r8lef"}]]),Qd="/assets/profilePhoto-C-Tb0G5u.jpg",Gd="/assets/heroImage-BQIENNd-.png",Kd=()=>{const e={bgLight:"bg-[#FDF8F0]",bgAccent:"bg-[#E7D6CB]",textPrimary:"text-[#3E2723]",textAccent:"text-[#6C4B40]",accentGold:"text-[#D4AF37]"},t=m.jsx("style",{children:`
+ */const Wd=kl("GraduationCap",[["path",{d:"M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z",key:"j76jl0"}],["path",{d:"M22 10v6",key:"1lu8f3"}],["path",{d:"M6 12.5V16a6 3 0 0 0 12 0v-3.5",key:"1r8lef"}]]),Qd="/Portfolio/assets/profilePhoto-C-Tb0G5u.jpg",Gd="/Portfolio/assets/heroImage-BQIENNd-.png",Kd=()=>{const e={bgLight:"bg-[#FDF8F0]",bgAccent:"bg-[#E7D6CB]",textPrimary:"text-[#3E2723]",textAccent:"text-[#6C4B40]",accentGold:"text-[#D4AF37]"},t=m.jsx("style",{children:`
         @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;700&family=Lora:wght@400;700&display=swap');
         .font-cormorant { font-family: 'Cormorant Garamond', serif; }
         .font-lora { font-family: 'Lora', serif; }
